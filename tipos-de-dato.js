5e75dca71451ca4a6fac86f81dde784f45ee0165
Elías Martínez Sánchez <em2317245@gmail.com>
2025-04-15 10:01:56 -0600

    datos_propuestos_por_actividad

--- tipos-de-dato.js
+++ tipos-de-dato.js
@@ -1,3 +1,21 @@
-console.log(typeof 42);
+console.log(typeof 42); // number
+
+console.log(typeof 'veinticinco'); // string
+
+console.log(typeof -666); // number
+
+console.log(typeof true); // boolean
+
+console.log(typeof 0); // number
+
+console.log(typeof ""); // string
+
+console.log(typeof null); // object
+
+console.log(typeof undefined); // undefined
+
+console.log(typeof FALSE); // ReferenceError: FALSE is not defined
+
+
 
 
