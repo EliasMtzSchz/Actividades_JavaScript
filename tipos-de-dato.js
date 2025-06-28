141e60002216dcd93659e167b887080fe0978b7a
Elías Martínez Sánchez <em2317245@gmail.com>
2025-06-27 20:37:27 -0600

    datos_propuestos_por_mi

--- tipos-de-dato.js
+++ tipos-de-dato.js
@@ -14,8 +14,18 @@ console.log(typeof null); // object
 
 console.log(typeof undefined); // undefined
 
-console.log(typeof FALSE); // ReferenceError: FALSE is not defined
+console.log(typeof false); // boolean
 
+console.log(typeof NaN); // number
 
+console.log(typeof Infinity); // number
 
+console.log(typeof null); // object
 
+console.log(typeof and); // ReferenceError: and is not defined 
+
+console.log(typeof 3.14); // number
+
+console.log(typeof true); // boolean
+
+console.log(typeof "cadena"); // string
